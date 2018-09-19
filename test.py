bb1d846f79eeac4fa37a8a2142c7ec81a19781f7
haha3564949 <haha3564949@126.com>
2018-09-19 11:02:47 +0800

    12

--- test.py
+++ test.py
@@ -4,15 +4,6 @@ import talib as tb
 import pandas as pd
 
 
-# d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-#     'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
-#
-# df = pd.DataFrame(d)
-# print df
-#
-#
-# df1=pd.DataFrame([{'rzche': 111864384L, 'securityAbbr': u'\u6d1b\u9633\u94bc\u4e1a', 'rqmcl': 564700L, 'opDate': u'2018-08-28', 'rqyl': 1868400L, 'rzye': 2130516160L, 'rqchl': 256200L, 'stockCode': u'603993', 'rzmre': 113198250L, u'"index"': 5510L}, {'rzche': 66038011L, 'securityAbbr': u'\u6d1b\u9633\u94bc\u4e1a', 'rqmcl': 151200L, 'opDate': u'2018-08-27', 'rqyl': 1559900L, 'rzye': 2129182294L, 'rqchl': 94400L, 'stockCode': u'603993', 'rzmre': 85196955L, u'"index"': 5511L}])
-# print df1.iloc[:,0].size
 
 import pandas as pd
 
