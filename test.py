309b3756f2445d2270b2cb8cfee7622c5cf7d9bf
haha3564949 <haha3564949@126.com>
2018-09-19 13:11:08 +0800

    12

--- test.py
+++ test.py
@@ -33,12 +33,12 @@ def calc_MACD(df, short=12, long=26, M=9):
     emas = calc_EMA(df, short)
     emaq = calc_EMA(df, long)
     temp = pd.Series(emas) - pd.Series(emaq)
-    df['diff']=temp
+    df['diff']=list(temp)
     for i in range(len(df)):
        if i==0:
            df.ix[i,'dea'] = df.ix[i,'diff']
        if i>0:
             df.ix[i,'dea'] = ((M-1)*df.ix[i-1,'dea'] + 2*df.ix[i,'diff'])/(M+1)
-    df['macd'] = 2*(df['diff'] - df['dea'])
+    df['macd'] = 2*(df['diff']- df['dea'])
     return df
 calc_MACD(df, 12, 26, 9).to_csv("ab.csv")
