6873016789d778f4734b8df775b19e45e0af7942
haha3564949 <haha3564949@126.com>
2019-01-02 18:36:32 +0800

    Merge branch 'master' of https://github.com/haha3564949/workshop into master1
    # Conflicts:
    #	aa.py

--- getallcode.py
+++ getallcode.py
@@ -14,13 +14,17 @@ def getInitWebData(startnum,endnum):
     for i in range(startnum,endnum+1):
         delta = datetime.timedelta(days=i)
         tempday = (mydate - delta).strftime('%Y-%m-%d')
-        df=ts.sz_margin_details(date=tempday,retry_count=3,pause=0.01)
-        if len(df)>0:
-            df1=df.iloc[:,[1,2,3,7,10,11]]
-            df1['rqye']=df1['rqye'].replace('--', '-1');
-            df1['opDate'] = pd.to_datetime(df1['opDate'], format='%Y-%m-%d %H:%M:%S')
-            df1.to_sql('rzrqtemp', con=engine, if_exists='append', chunksize=100, index=False,dtype={'stockCode':VARCHAR2(20),'securityAbbr':VARCHAR2(20),'rqye':FLOAT,'rzye':FLOAT});
-            print i
+        try:
+            df=ts.sz_margin_details(date=tempday,retry_count=3,pause=0.01)
+            if len(df)>0:
+                df1=df.iloc[:,[1,2,3,7,10,11]]
+                df1['rqye']=df1['rqye'].replace('--', '-1');
+                df1['opDate'] = pd.to_datetime(df1['opDate'], format='%Y-%m-%d %H:%M:%S')
+                df1.to_sql('rzrqtemp', con=engine, if_exists='append', chunksize=100, index=False,dtype={'stockCode':VARCHAR2(20),'securityAbbr':VARCHAR2(20),'rqye':FLOAT,'rzye':FLOAT});
+                print i
+        except Exception,e:
+            print("Error:",e)
+            print tempday
 def getInitPriceData(startnum,endnum):
     mydate = datetime.datetime.today()
     startdelta = datetime.timedelta(days=startnum)
@@ -43,28 +47,29 @@ def updateInitWebData(sql):
     except Exception, e:
         print e.message
 
+
 def getDBData():
 
     df1 = pd.read_sql(
-        """ select distinct  a."stockCode"  from szrzrq a  """,
+        """ select distinct  a."stockCode"  from rzrqtemp a where a.rqye is not null """,
         engine)
-    dftemp = pd.DataFrame(columns=['stockCode', 'securityAbbr', 'rzrqye', 'opDate', 'ema', 'emas', 'emaq', 'diff', 'dea', 'macd'])
+    dftemp = pd.DataFrame(columns=['stockCode', 'securityAbbr', 'rqye', 'opDate', 'ema', 'emas', 'emaq', 'diff', 'dea', 'macd'])
     for str in df1.iloc[:,0]:
         df2 = pd.read_sql(
-            """select  a.* from szrzrq a where dbms_lob.substr(a."stockCode")=:a  order by dbms_lob.substr(a."opDate")  asc""",
+            """select  a.* from rzrqtemp a where a.\"stockCode\"=:a  and a.rqye is not null order by a.\"opDate\"  asc""",
             engine, params={"a": str})
         df3=calc_MACD(df2)
         dftemp=dftemp.append(df3)
 
-    dftemp.to_sql('myszrzrq', con=engine, if_exists='append', chunksize=100, index=False)
+    dftemp.to_sql('rzrqall', con=engine, if_exists='append', chunksize=100, index=False,dtype={'stockCode':VARCHAR2(6),'securityAbbr':VARCHAR2(20)} )
 
 
 def calc_EMA(df, N):
     for i in range(len(df)):
         if i==0:
-            df.ix[i,'ema']=int(df.ix[i,'rzrqye'])
+            df.ix[i,'ema']=int(df.ix[i,'rqye'])
         if i>0:
-            df.ix[i,'ema']=((N-1)*int(df.ix[i-1,'ema'])+2*int(df.ix[i,'rzrqye']))/(N+1)
+            df.ix[i,'ema']=((N-1)*int(df.ix[i-1,'ema'])+2*int(df.ix[i,'rqye']))/(N+1)
     ema=list(df['ema'])
     return ema
 
@@ -86,15 +91,12 @@ def calc_MACD(df, short=12, long=26, M=9):
 def main():
     # every day before 9:00 AM ,else the trade should be exchanged to settlement
     startnum=1
-    endnum=2
+    endnum=100
     getInitWebData(startnum, endnum);
     getInitPriceData(startnum, endnum);
-    # updateInitWebData("merge into rzrqtemp rzt "
-    #                   " using rzrqprice rzp  on (rzt.\"stockCode\" = rzp.\"stockCode\" and rzt.\"opDate\" = rzp.\"date\"  and  rzt.rzye<0  "
-    #                   "        ) "
-    #                   "   when matched then "
-    #                   " update set rzt.rqye = rzt.rzye+rzt.rqyl*(rzp.close) ");
-
+    updateInitWebData("update rzrqtemp rzt set rzt.rqye = rzt.rzye+rzt.rqyl*(select rzp.close from rzrqprice rzp where rzt.\"stockCode\" = rzp.\"stockCode\" and rzt.\"opDate\" = rzp.\"date\") "
+                      "    where  rzt.rqye<0 ");
+    getDBData()
 
 
 
